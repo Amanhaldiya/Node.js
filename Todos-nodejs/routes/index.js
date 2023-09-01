85faa7ad712d854f87cfc42d430d21c577f8af1e
Aman Haldiya <95669187+Amanhaldiya@users.noreply.github.com>
2023-09-02 00:32:22 +0530

    Update index.js

--- Todos-nodejs/routes/index.js
+++ Todos-nodejs/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 const dashboardController = require('../controllers/dashboardController');
-const registerController = require('../controllers/registerController');
 const alltaskController = require('../controllers/alltaskController');
 const completedtaskController = require('../controllers/completedtaskController');
 
@@ -11,7 +10,6 @@ console.log('Router loaded');
 
 router.get('/', homeController.home);
 router.get('/dashboard', dashboardController.dashboard)
-router.get('/register', registerController.register);
 router.get('/alltask', alltaskController.alltask);
 router.get('/completedtask', completedtaskController.completedtask);
 

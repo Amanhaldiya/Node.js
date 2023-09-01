e77952939e065ce7b3be2a7a5c3c36ef736f7853
Aman Haldiya <95669187+Amanhaldiya@users.noreply.github.com>
2023-09-02 00:40:57 +0530

    Update dashboardController.js

--- Todos-nodejs/controllers/dashboardController.js
+++ Todos-nodejs/controllers/dashboardController.js
@@ -5,14 +5,15 @@ const Dashboard = require('../models/dashboard');
 module.exports.dashboard = function(req, res){
     const data = Dashboard.find({})
     .then(function(data){
-        User.findOne({email : "Amanhaldiya512@gmail.com"})
-        .then(function(user){
+      //  User.findOne({email : "Amanhaldiya512@gmail.com"})
+      //  .then(function(user){
         return res.render('dashboard', {
             title: "Dashboard",
             // name: user.name,
             dashboard: data
-        });
-    })
+        }
+                         );
+    
     })
     .catch(function(err){
         console.log('Error', err);

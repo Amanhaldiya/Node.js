df427100dc6125e97851f306ceb1a1e873cd6a6c
Aman Haldiya <95669187+Amanhaldiya@users.noreply.github.com>
2023-09-02 00:46:48 +0530

    Update alltaskController.js

--- Todos-nodejs/controllers/alltaskController.js
+++ Todos-nodejs/controllers/alltaskController.js
@@ -5,15 +5,15 @@ const Dashboard = require('../models/dashboard');
 module.exports.alltask = function(req, res){
     const data = Dashboard.find({})
     .then(function(data){
-        User.findOne({email : "Amanhaldiya512@gmail.com"})
-        .then(function(user){
+    //    User.findOne({email : "Amanhaldiya512@gmail.com"})
+      //  .then(function(user){
           //  console.log(`**********user`, user.name);
         return res.render('alltask', {
             title: "Dashboard",
           //  name: user.name,
             dashboard: data
         });
-    })
+//    })
     })
     .catch(function(err){
         console.log('Error', err);
